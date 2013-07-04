2174d2ae7b4e2815405767d162e0f304f7f0dbab
Jim Young <jyoung@sapphire-llc.com>
2013-07-04 21:44:07 +0100

    Finished sign in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+	def new
+  end
+
+  def create
+  	user = User.find_by_email(params[:session][:email].downcase)
+  	if (user && user.authenticate(params[:session][:password]))
+  		#sign in the user
+  		sign_in user
+  		redirect_to user
+  	else
+  		#create error message and re-rener the sign-in form
+  		flash.now[:error] = 'Invalid email/password combination'
+    	render 'new'
+  	end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_url
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -26,6 +26,11 @@ describe "UserPages" do
       it "should not create a user" do
         expect {click_button submit}.not_to change(User, :count)
       end
+      describe "after_submission" do
+        before {click_button submit}
+        it {should have_selector('title', text: 'Sign up')}
+        it {should have_content('error')}
+      end
     end
 
     describe "with valid information" do
@@ -38,6 +43,11 @@ describe "UserPages" do
 
       it "should create a user" do
         expect {click_button submit}.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        it {should have_link('Sign out')}
       end
     end
   end
